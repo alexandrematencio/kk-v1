46552a67a73cc96beceb45d32ac13adeb49c1194
Alexandre Matencio <amatencio@pm.me>
2026-05-20 12:25:56 +0200

    deploy

---
+++ _nuxt/Nmte2ADA.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./B9VDcsMf.js";import{_ as a,o as s,c,a as n,b as _,G as d}from"./B0DYHfPy.js";const r={},l={class:"layout"},f={class:"layout-nav"};function p(e,u){const o=t;return s(),c("div",l,[n("div",f,[_(o)]),d(e.$slots,"default",{},void 0,!0)])}const v=a(r,[["render",p],["__scopeId","data-v-9d660de8"]]);export{v as default};
